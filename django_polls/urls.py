f9a41c1e024f723e22c455155dcdc3233eac0127
Salman Farooq <salman.farooq3310@gmail.com>
2022-06-11 15:05:10 +0500

    message

--- django_polls/urls.py
+++ django_polls/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
+    path('', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
 
